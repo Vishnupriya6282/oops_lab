c5a39465c348f667d6080a014c445f8421e3d11b
Vishnupriya6282 <149034706+Vishnupriya6282@users.noreply.github.com>
2024-05-05 15:50:01 +0530

    Create InnerClass.java

---
+++ InnerClass.java
@@ -0,0 +1,40 @@
+class Cpu
+{
+	
+	int price = 40000;
+	class Processor
+	{
+		int core = 16;
+		String name = "Ryzen";
+		void display()
+		{
+			System.out.println("Price : " + price );
+			System.out.println("Core : " + core );
+			System.out.println("Brand : " + name );
+		}
+	}
+	
+	static class Ram
+	{
+		int size = 16;
+		String name = "Adata";
+		void display()
+		{
+			System.out.println("Size : " + size );
+			System.out.println("Brand : " + name );
+		}
+	}
+}
+
+
+ public class InnerClass
+{
+	public static void main(String args[])
+	{
+		Cpu c = new Cpu();
+		Cpu.Processor p = c.new Processor();
+		p.display();
+		Cpu.Ram r = new Cpu.Ram();
+		r.display();
+	}
+}
