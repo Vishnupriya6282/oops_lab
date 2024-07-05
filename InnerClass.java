9674e0aef95a98e913fd5329829b0157df23dce4
Vishnupriya6282 <149034706+Vishnupriya6282@users.noreply.github.com>
2024-07-05 21:28:06 +0530

    InnerClass.java

--- InnerClass.java
+++ InnerClass.java
@@ -1,40 +1,34 @@
-class Cpu
-{
-	
-	int price = 40000;
-	class Processor
-	{
-		int core = 16;
-		String name = "Ryzen";
-		void display()
-		{
-			System.out.println("Price : " + price );
-			System.out.println("Core : " + core );
-			System.out.println("Brand : " + name );
-		}
-	}
-	
-	static class Ram
-	{
-		int size = 16;
-		String name = "Adata";
-		void display()
-		{
-			System.out.println("Size : " + size );
-			System.out.println("Brand : " + name );
-		}
-	}
+class Cpu {
+int price;
+public Cpu(int price) {
+this.price = price;
 }
 
-
- public class InnerClass
-{
-	public static void main(String args[])
-	{
-		Cpu c = new Cpu();
-		Cpu.Processor p = c.new Processor();
-		p.display();
-		Cpu.Ram r = new Cpu.Ram();
-		r.display();
-	}
+class Processor {
+int core;
+String name;
+Processor(int core, String name) {
+this.core = core;
+this.name = name;
+}
+void display() {
+System.out.println("Price : " + price);
+System.out.println("Core : " + core);
+System.out.println("Brand : " + name);
+}
+}
+static class Ram {
+ static void display(int size,String name) {
+System.out.println("Size : " + size);
+System.out.println("Brand : " + name);
+}
+}
+}
+ public class Innerclass {
+public static void main(String args[]) {
+Cpu c = new Cpu(70000);
+Cpu.Processor p = c.new Processor(16, "Intel");
+p.display();
+Cpu.Ram.display(16,"Adata");
+}
 }
